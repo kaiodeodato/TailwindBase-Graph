933b39beee755651151a27e677269020c2d54a91
kaiodeodato <kaiod.oficial@gmail.com>
2024-04-23 09:11:55 +0100

    first after change

---
+++ src/script.js
@@ -0,0 +1,31 @@
+const fetchData = async () => {
+    const url = 'https://nodegraphql.onrender.com/';
+    const query = `
+      {
+        publishers {
+          company
+          booksByPublisher {
+            title
+          }
+        }
+      }
+    `;
+  
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query }),
+      });
+  
+      const data = await response.json();
+      console.log(data.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  
+  fetchData();
+  
